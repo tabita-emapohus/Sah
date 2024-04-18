8bfab4d4871c6e23d4e01984546d45fed16a0a24
tabita-emapohus <161648276+tabita-emapohus@users.noreply.github.com>
2024-04-18 23:15:40 +0300

    add squares

---
+++ script.js
@@ -0,0 +1,107 @@
+function setup() {
+    createCanvas(600, 600)
+    generateCirclePositions()
+    keyPressed()
+}
+function draw() {
+    background("black");
+    drawsah();
+    smilyFaceTabita();
+}
+function drawsah() {
+    //
+    square(0, 0, 75)
+    fill("white")
+    //
+    square(150, 0, 75)
+    fill("white")
+    //
+    square(300, 0, 75)
+    //
+    square(450, 0, 75)
+    fill("white")
+    // 
+    square(75, 75, 75)
+    fill("white")
+    //
+    square(225, 75, 75)
+    fill("white")
+    // 
+    square(375, 75, 75)
+    fill("white")
+    //
+    square(525, 75, 75)
+    fill("white")
+    //
+    square(0, 150, 75)
+    fill("white")
+    //
+    square(150, 150, 75)
+    fill("white")
+    //
+    square(300, 150, 75)
+    fill("white")
+    //
+    square(450, 150, 75)
+    fill("white")
+    //
+    square(75, 225, 75)
+    fill("white")
+    // 
+    square(225, 225, 75)
+    fill("white")
+    //
+    square(375, 225, 75)
+    fill("white")
+    //
+    square(525, 225, 75)
+    fill("white")
+    //
+    square(0, 300, 75)
+    fill("white")
+    // 
+    square(150, 300, 75)
+    fill("white")
+    //
+    square(300, 300, 75)
+    fill("white")
+    // 
+    square(450, 300, 75)
+    fill("white")
+    //
+    square(75, 375, 75)
+    fill("white")
+    //
+    square(225, 375, 75)
+    fill("white")
+    //
+    square(375, 375, 75)
+    fill("white")
+    // 
+    square(525, 375, 75)
+    fill("white")
+    //
+    square(0, 450, 75)
+    fill("white")
+    //
+    square(150, 450, 75)
+    fill("white")
+    // 
+    square(300, 450, 75)
+    fill("white")
+    //
+    square(450, 450, 75)
+    fill("white")
+    //
+    square(75, 525, 75)
+    fill("white")
+    //
+    square(225, 525, 75)
+    fill("white")
+    //
+    square(375, 525, 75)
+    fill("white")
+    //
+    square(525, 525, 75)
+    fill("white")
+}
