d30d0eafd3bceb5c506f818b5b0b2e613f19d304
tabita-emapohus <161648276+tabita-emapohus@users.noreply.github.com>
2024-04-18 23:16:28 +0300

    add circles

--- script.js
+++ script.js
@@ -105,3 +105,89 @@ function drawsah() {
     square(525, 525, 75)
     fill("white")
 }
+function smilyFaceTabita() {
+    //Body1
+    stroke("black");
+    fill("gray");
+    circle(33,190,50);
+
+    // 2
+    fill("gray");
+    stroke("black");
+    circle(115,114,50);
+
+     //3
+     fill("gray");
+     stroke("black");
+     circle(190,260,50);
+
+     //4
+     fill("gray");
+     stroke("black");
+     circle(263,114,50);
+
+     //5
+     fill("gray");
+     stroke("black");
+     circle(340,190,50);
+
+     //6
+     fill("gray");
+     stroke("black");
+     circle(415,114,50);
+     
+     //7
+     fill("gray");
+     stroke("black");
+     circle(490,260,50);
+
+     //8
+     fill("gray");
+     stroke("black");
+     circle(565,114,50);
+
+     //Body White
+     //1
+     fill("beige");
+     stroke("black");
+     circle(35,485,50);
+
+     //2
+     fill("beige");
+     stroke("black");
+     circle(115,340,50);
+
+     //3
+     fill("beige");
+     stroke("black");
+     circle(190,485,50);
+
+     //4
+     fill("beige");
+     stroke("black");
+     circle(260,410,50);
+
+
+     //5
+     fill("beige");
+     stroke("black");
+     circle(336,340,50);
+
+
+     //6
+     fill("beige");
+     stroke("black");
+     circle(415,415,50);
+
+
+     //7
+     fill("beige");
+     stroke("black");
+     circle(485,485,50);
+
+     //8
+     fill("beige");
+     stroke("black");
+     circle(565,410,50);
+
+}
